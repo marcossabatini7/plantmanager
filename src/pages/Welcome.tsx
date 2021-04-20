4a7115b4dfac48945a49200feaee7278cdd5049e
Marcos Sabatini <marcossabatini7@gmail.com>
2021-04-20 19:08:25 -0300

    Refatorando aplicação até aqui

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
-import { SafeAreaView, Text, Image, StyleSheet } from 'react-native'
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native'
 
 import wateringImg from '../assets/watering.png'
-import { Button } from '../components/Button'
 import colors from '../styles/colors'
 
 export function Welcome() {
@@ -12,14 +18,20 @@ export function Welcome() {
         Gerencie {'\n'}suas plantas {'\n'}de forma fácil
       </Text>
 
-      <Image source={wateringImg} style={styles.image}></Image>
+      <Image
+        source={wateringImg}
+        style={styles.image}
+        resizeMode='contain'
+      ></Image>
 
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
 
-      <Button title='>' />
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>{'>'}</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   )
 }
@@ -28,7 +40,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
   },
   title: {
     fontSize: 32,
@@ -37,14 +49,26 @@ const styles = StyleSheet.create({
     color: colors.heading,
     marginTop: 38,
   },
+  image: {
+    height: Dimensions.get('window').width * 0.7,
+  },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
   },
-  image: {
-    width: 292,
-    height: 284,
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
   },
 })
